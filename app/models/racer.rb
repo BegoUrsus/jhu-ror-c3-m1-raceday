721529405250f82be7564d51f485278a6295cdcd
BegoUrsus <layira2002@gmail.com>
2016-04-30 16:33:50 +0200

    created class method all

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class Racer
 	#convinience method for access to client in console
 	def self.mongo_client
@@ -7,5 +9,14 @@ class Racer
 	#convinience method for access to racer collection
 	def self.collection
 		self.mongo_client['racers']
+	end
+
+
+	def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
+		result=collection.find(prototype)
+			.sort(sort)
+			.skip(skip)
+    	result=result.limit(limit) if !limit.nil?
+	    return result	
 	end
 end
