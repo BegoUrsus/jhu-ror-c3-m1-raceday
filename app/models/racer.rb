574a22d82acb85830daeb59d138f01d690c9f247
BegoUrsus <layira2002@gmail.com>
2016-04-30 17:56:19 +0200

    added attributes to Racer class

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,12 @@
 require 'pp'
 
 class Racer
+	attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+	def to_s
+    "#{@id}: #{@number}, #{@first_name} #{@last_name}, #{@gender}, #{@group}, #{@secs}"
+  end
+
 	#convinience method for access to client in console
 	def self.mongo_client
 		Mongoid::Clients.default
