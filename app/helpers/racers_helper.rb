bb8310540de1e0dc457c3d3c2949ebe92191cbe4
BegoUrsus <layira2002@gmail.com>
2016-05-01 10:44:41 +0200

    added route to racer controller, set index page, create helper method toRacer and remove confirmation dialog from Destroy link

--- app/helpers/racers_helper.rb
+++ app/helpers/racers_helper.rb
@@ -1,2 +1,9 @@
 module RacersHelper
+	# Instance method toRacer. It must:
+	# - Accept a single input argument
+	# - If the type of the input argument is a Racer, simply return the instance unmodified. 
+	#   Else attempt to instantiate a Racer from the input argument and return the result.
+	def toRacer(value)
+		return value.is_a?(Racer) ? value : Racer.new(value)
+	end
 end
